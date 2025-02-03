3c282066d173b4d8d47ebccb013a636a7eb4a0e0
ZEY <1010642166@qq.com>
2025-02-03 23:03:04 +0800

    init JavaScript section

---
+++ JavaScript/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,32 @@
+/*
+let js = "amazing";
+
+// console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let PI = 3.14159;
+
+let $function = 27;
+let myFirstJob = "programmer";
+let myCurrentJob = "teacher";
+
+let age;
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
