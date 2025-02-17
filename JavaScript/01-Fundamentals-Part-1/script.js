8c0c07814b96297950c8f80453f2f43826adc100
ZEY <1010642166@qq.com>
2025-02-17 15:09:55 +0800

    01-Fundamentals-Part-1: learn basic operators in JavaScript

--- JavaScript/01-Fundamentals-Part-1/script.js
+++ JavaScript/01-Fundamentals-Part-1/script.js
@@ -1,4 +1,5 @@
 /*
+// ----------------------Variables---------------------- //
 let js = "amazing";
 
 // console.log(40 + 8 + 23 - 10);
@@ -22,6 +23,7 @@ let age;
 
 console.log(myFirstJob);
 
+// ----------------------Data Types---------------------- //
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -41,8 +43,8 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
+// ----------------------const let And var---------------------- //
 let age = 30;
 age = 31;
 
@@ -53,6 +55,38 @@ const birthYear = 1991;
 
 var job = "programmer";
 job = "teacher";
+lastName = "Zhang";
+console.log(lastName);
+*/
 
-lastNeme = "Zhang";
-console.log(lastNeme);
+// ----------------------Operators---------------------- //
+//Math operators
+const now = 2037;
+const ageZhang = now - 2003;
+const ageSarah = now - 2018;
+console.log(ageZhang, ageSarah);
+
+console.log(ageZhang * 2, ageZhang / 10, 2 ** 3);
+
+const firstName = "Ey";
+const lastName = "Zhang";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1
+x--; // 99
+console.log(x);
+console.log(x++); //print 99
+console.log(++x); //print 101
+
+// Comparison operators
+console.log(ageZhang > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
