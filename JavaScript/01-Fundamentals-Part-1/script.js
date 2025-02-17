771d3541b2bb40234174e5603e2d30bfe3d14676
ZEY <1010642166@qq.com>
2025-02-17 21:32:13 +0800

    01-Fundamentals-Part-1: learn == and === operators

--- JavaScript/01-Fundamentals-Part-1/script.js
+++ JavaScript/01-Fundamentals-Part-1/script.js
@@ -227,11 +227,9 @@ console.log("23" / "2");
 let n = "1" + 1; //11
 n = n - 1; //10
 console.log(n);
-*/
 
 // ---------------------- Truthy and Falsy values ---------------------- //
 // 5个假值： null, undefined, 0, NaN, ""
-
 console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean("Jonas"));
@@ -251,3 +249,25 @@ if (height) {
 } else {
   console.log("height is UNDEFINED!");
 }
+*/
+
+// ---------------------- == and ===  ---------------------- //
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favorite === 7) {
+  console.log("7 is also a cool number!");
+} else if (favorite === 9) {
+  console.log("9 is also a cool number!");
+} else {
+  console.log("Number is not 23 or 7 or 9!");
+}
+
+if (favorite !== 23) console.log("Wht not 23?");
