ad97e38a27ea2c1eab5b5ba7cecd9382df2ed639
ZEY <1010642166@qq.com>
2025-02-19 23:41:16 +0800

    02-Fundamentals-Part-2: finish the CHALLENGE 1 in section-02

--- JavaScript/02-Fundamentals-Part-2/script.js
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -1,15 +1,14 @@
 /*jshint -W097 */
 "use strict";
-/*
-let hasDriversLicense = false;
-const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log("I can drive :D");
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
-*/
 
 // ---------------------- Function ---------------------- //
 // function logger() {
@@ -48,21 +47,84 @@ if (hasDriversLicense) console.log("I can drive :D");
 // console.log(age1, age2);
 
 //Function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+//
+// //Arrow function
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+//
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   // return retirement;
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+//
+// console.log(yearsUntilRetirement(1991, "Ey"));
+// console.log(yearsUntilRetirement(1980, "Bob"));
+
+// functions call other function
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+//
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//
+//   const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+//   return juice;
+// }
+//
+// console.log(fruitProcessor(2, 3));
+
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+//
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years.`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired.`);
+//     return -1;
+//   }
+//   // return `${firstName} retires in ${retirement} years.`;
+// };
+// console.log(yearsUntilRetirement(1991, "Ey"));
+// console.log(yearsUntilRetirement(1950, "Mike"));
+
+// ---------------------- CHALLENGE 1 ---------------------- //
+const calcAverage = (score1, score2, score3) => {
+  return (score1 + score2 + score3) / 3;
 };
 
-//Arrow function
-const calcAge3 = (birthYear) => 2037 - birthYear;
-const age3 = calcAge3(1991);
-console.log(age3);
-
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  // return retirement;
-  return `${firstName} retires in ${retirement} years.`;
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins > avgKoalas) {
+    return avgDolphins >= 2 * avgKoalas
+      ? `Dolphins win (${avgDolphins} vs. ${avgKoalas})`
+      : "No one wins";
+  } else {
+    return avgKoalas >= 2 * avgDolphins
+      ? `koalas win (${avgDolphins} vs. ${avgKoalas})`
+      : "No one wins";
+  }
 };
 
-console.log(yearsUntilRetirement(1991, "Ey"));
-console.log(yearsUntilRetirement(1980, "Bob"));
+// Test 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+console.log(checkWinner(scoreDolphins, scoreKoalas));
