8c7d974b7503a03c8c61e92d875918c2d8ea6636
ZEY <1010642166@qq.com>
2025-02-21 15:53:04 +0800

    02-Fundamentals-Part-2: learn for-loop in JS and supplement the background of challenges

--- JavaScript/02-Fundamentals-Part-2/script.js
+++ JavaScript/02-Fundamentals-Part-2/script.js
@@ -101,6 +101,25 @@
 // console.log(yearsUntilRetirement(1950, "Mike"));
 
 // ------------------------------- CHALLENGE 1 ------------------------------- //
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
 // const calcAverage = (score1, score2, score3) => {
 //   return (score1 + score2 + score3) / 3;
 // };
@@ -205,6 +224,20 @@
 // }
 
 // ------------------------------- CHALLENGE 2 ------------------------------- //
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
 // const bills = [125, 555, 44];
 // const calcTip = function (bill) {
 //   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
@@ -291,36 +324,64 @@
 // console.log(ey.getSummary());
 
 // ------------------------------- CHALLENGE 3 ------------------------------- //
-const mark = {
-  firstName: "Mark",
-  lastName: "Miller",
-  height: 1.69,
-  weight: 78,
-  calcBMI: function () {
-    this.bmi = this.weight / this.height ** 2;
-    return this.bmi;
-  },
-};
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
 
-const john = {
-  firstName: "John",
-  lastName: "Smith",
-  height: 1.95,
-  weight: 92,
-  calcBMI: function () {
-    this.bmi = this.weight / this.height ** 2;
-    return this.bmi;
-  },
-};
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
 
-if (mark.calcBMI() > john.calcBMI()) {
-  console.log(
-    `${mark.firstName} ${mark.lastName}'s BMI (${mark.bmi}) is higher than ${john.firstName} ${john.lastName}'s BMI (${john.bmi})`,
-  );
-} else if (mark.calcBMI() < john.calcBMI()) {
-  console.log(
-    `${john.firstName} ${john.lastName}'s BMI (${john.bmi}) is higher than ${mark.firstName} ${mark.lastName}'s BMI (${john.bmi})`,
-  );
-} else {
-  console.log("Draw! ");
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+// const mark = {
+//   firstName: "Mark",
+//   lastName: "Miller",
+//   height: 1.69,
+//   weight: 78,
+//   calcBMI: function () {
+//     this.bmi = this.weight / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+//
+// const john = {
+//   firstName: "John",
+//   lastName: "Smith",
+//   height: 1.95,
+//   weight: 92,
+//   calcBMI: function () {
+//     this.bmi = this.weight / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+//
+// if (mark.calcBMI() > john.calcBMI()) {
+//   console.log(
+//     `${mark.firstName} ${mark.lastName}'s BMI (${mark.bmi}) is higher than ${john.firstName} ${john.lastName}'s BMI (${john.bmi})`,
+//   );
+// } else if (mark.calcBMI() < john.calcBMI()) {
+//   console.log(
+//     `${john.firstName} ${john.lastName}'s BMI (${john.bmi}) is higher than ${mark.firstName} ${mark.lastName}'s BMI (${john.bmi})`,
+//   );
+// } else {
+//   console.log("Draw! ");
+// }
+
+// ------------------------------- For loop ------------------------------- //
+// console.log("Lifting weights repetition 1 🏋️");
+// console.log("Lifting weights repetition 2 🏋️");
+// console.log("Lifting weights repetition 3 🏋️");
+// console.log("Lifting weights repetition 4 🏋️");
+// console.log("Lifting weights repetition 5 🏋️");
+// console.log("Lifting weights repetition 6 🏋️");
+// console.log("Lifting weights repetition 7 🏋️");
+// console.log("Lifting weights repetition 8 🏋️");
+// console.log("Lifting weights repetition 9 🏋️");
+// console.log("Lifting weights repetition 10 🏋️");
+
+// for loop keeps running while the condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️`);
 }
