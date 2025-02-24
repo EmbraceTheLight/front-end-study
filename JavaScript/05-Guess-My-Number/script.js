8acb72c584a1b5eb3239f115fc297c7cb93ff8fc
ZEY <1010642166@qq.com>
2025-02-24 15:47:34 +0800

    feat(05-Guess-My-Number): learn the DOM selector and event listener, and add a little logic of guessing game

--- JavaScript/05-Guess-My-Number/script.js
+++ JavaScript/05-Guess-My-Number/script.js
@@ -1,4 +1,21 @@
 /* jshint -W097 */
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Correct Number!";
+//
+// document.querySelector(".number").textContent = 13;
+// console.log(typeof document.querySelector(".number").textContent);
+// document.querySelector(".score").textContent = 10;
+//
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+  }
+});
