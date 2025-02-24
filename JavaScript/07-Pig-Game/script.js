61de65f7215eb7daa1de61ff54d89c1d995166f2
ZEY <1010642166@qq.com>
2025-02-25 01:09:49 +0800

    feat(07-Pig-Game): Add event listener to the 'New-Game' button. Finish the Pig-Game logic 🫡.

--- JavaScript/07-Pig-Game/script.js
+++ JavaScript/07-Pig-Game/script.js
@@ -14,15 +14,31 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// 初始状态
-score0El.textContent = "0";
-score1El.textContent = "0";
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  // 重置游戏状态
+  scores = [0, 0];
+  currentScore = 0;
+  playing = true;
+  activePlayer = 0;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  diceEl.classList.add("hidden");
+
+  // 重置分数
+  scores[0] = scores[1] = 0;
+  currentScore = 0;
+  current0El.textContent = "0";
+  current1El.textContent = "0";
+  score0El.textContent = "0";
+  score1El.textContent = "0";
+
+  //移除获胜者样式，并重置当前玩家为第一个玩家
+  document.querySelector(`.player--0`).classList.remove("player--winner");
+  document.querySelector(`.player--1`).classList.remove("player--winner");
+  document.querySelector(`.player--1`).classList.remove("player--active");
+  document.querySelector(`.player--0`).classList.add("player--active");
+};
+init();
 
 // 切换到下一个玩家
 const switchPlayer = function () {
@@ -83,3 +99,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
