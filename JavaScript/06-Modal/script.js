7a134db0d3be202262b20b01d6a67d14adf4ff45
ZEY <1010642166@qq.com>
2025-02-24 21:25:06 +0800

    feat(06-Modal): Learn how to deal with classes and add keyboard event listener to the Modal. Now, this Model is completely finished 🫡.

--- JavaScript/06-Modal/script.js
+++ JavaScript/06-Modal/script.js
@@ -5,10 +5,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -24,3 +22,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
