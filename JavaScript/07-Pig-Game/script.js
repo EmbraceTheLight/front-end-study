87034d5e7b726b8b9f6b2372eccc599fe67b8af9
ZEY <1010642166@qq.com>
2025-02-24 23:46:25 +0800

    feat(07-Pig-Game): initial the game interface

--- JavaScript/07-Pig-Game/script.js
+++ JavaScript/07-Pig-Game/script.js
@@ -1,2 +1,12 @@
 /* jshint -W097 */
 "use strict";
+
+// 选择元素
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+// 初始状态
+score0El.textContent = "0";
+score1El.textContent = "0";
+diceEl.classList.add("hidden");
