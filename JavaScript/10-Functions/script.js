d9b56843187a63f5d2f7486ede6526b2c5c2f53f
ZEY <1010642166@qq.com>
2025-03-06 16:28:13 +0800

    10-Function: learn arguments are all passed into functions by value in JavaScript

--- JavaScript/10-Functions/script.js
+++ JavaScript/10-Functions/script.js
@@ -2,27 +2,57 @@
 "use strict";
 
 // ------------------------------------ Function parameters ------------------------------------ //
-const bookings = [];
-const createBooking = function (
-  flightNumber,
-  numPassengers = 1,
-  price = 199 * numPassengers,
-) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
-  const booking = {
-    flightNumber,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// const bookings = [];
+// const createBooking = function (
+//   flightNumber,
+//   numPassengers = 1,
+//   price = 199 * numPassengers,
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+//   const booking = {
+//     flightNumber,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+//
+// createBooking("LH123");
+// createBooking("LH123", 2, 800);
+// createBooking("LH123", 2);
+// createBooking("LH123", 5);
+//
+// createBooking("LH123", undefined, 1000);
+
+// ------------------------------------ The way to pass arguments ------------------------------------ //
+const flight = "LH123";
+const ey = {
+  name: "Ey",
+  passport: 510526405602,
 };
 
-createBooking("LH123");
-createBooking("LH123", 2, 800);
-createBooking("LH123", 2);
-createBooking("LH123", 5);
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr." + passenger.name;
 
-createBooking("LH123", undefined, 1000);
+  if (passenger.passport === 510526405602) {
+    alert("Check in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+checkIn(flight, ey);
+console.log(flight);
+console.log(ey);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000000);
+};
+
+newPassport(ey);
+console.log(ey);
+checkIn(flight, ey);
