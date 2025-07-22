4d364d6064b8b48aef7bc6e72479d684f98ae952
zhangenyang <enyang.zhang@simtek.cc>
2025-07-22 13:27:32 +0800

    11-Array-Bankist: learn the forEach method of array

--- JavaScript/11-Arrays-Bankist/script.js
+++ JavaScript/11-Arrays-Bankist/script.js
@@ -72,7 +72,7 @@ const currencies = new Map([
   ["GBP", "Pound sterling"],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -117,7 +117,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // // JOIN method
 // console.log(letters.join(" - "));
 
-// ------------------------------------ Array methods: at ---------- //
+// ------------------------------------ Array methods: at ------------------------------------ //
 // const arr = [23, 11, 64];
 // console.log(arr[0]); // 23
 // console.log(arr.at(0)); // 23
@@ -130,3 +130,27 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // // at method can also be used with strings
 // console.log("hunterETL".at(0)); // "h"
 // console.log("hunterETL".at(-1)); // "L"
+
+// ------------------------------------ Array methods: forEach ------------------------------------ //
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//
+// for (const [i,movement] of movements.entries()) {
+//   if (movement>0){
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   }else{
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+//   }
+// }
+//
+// console.log('---- FOREACH ----')
+// movements.forEach(function(mov,i,arr){
+//   if (mov>0){
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   }else{
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+//   }
+// })
+// // 0: function(200)
+// // 1: function(450)
+// // 1: function(-400)
+// // ...
