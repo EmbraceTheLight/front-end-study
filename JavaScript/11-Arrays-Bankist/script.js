fa96a42deb89aa0836c5d31f61ab6d9f585c4394
ZEY <1010642166@qq.com>
2025-07-22 23:42:47 +0800

    11-Array-Bankist: learn forEach method of map and set

--- JavaScript/11-Arrays-Bankist/script.js
+++ JavaScript/11-Arrays-Bankist/script.js
@@ -66,12 +66,6 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -134,23 +128,41 @@ const currencies = new Map([
 // ------------------------------------ Array methods: forEach ------------------------------------ //
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //
-// for (const [i,movement] of movements.entries()) {
-//   if (movement>0){
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
 //     console.log(`Movement ${i + 1}: You deposited ${movement}`);
-//   }else{
-//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`)
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
 //   }
 // }
 //
-// console.log('---- FOREACH ----')
-// movements.forEach(function(mov,i,arr){
-//   if (mov>0){
+// console.log("---- FOREACH ----");
+// movements.forEach(function (mov, i, arr) {
+//   if (mov > 0) {
 //     console.log(`Movement ${i + 1}: You deposited ${mov}`);
-//   }else{
-//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`)
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
 //   }
-// })
+// });
 // // 0: function(200)
 // // 1: function(450)
 // // 1: function(-400)
 // // ...
+// /* forEach in set and map */
+// // map
+// const currencies = new Map([
+//   ["USD", "United States dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound sterling"],
+// ]);
+//
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+//
+// // set
+// const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
