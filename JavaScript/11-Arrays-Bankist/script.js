895fc6638782e48fad1e6ca75492f4e08351d68e
ZEY <1010642166@qq.com>
2025-07-23 23:47:37 +0800

    11-Array-Bankist: learn creat and insert a DOM element

--- JavaScript/11-Arrays-Bankist/script.js
+++ JavaScript/11-Arrays-Bankist/script.js
@@ -62,6 +62,24 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovements = function (movements) {
+  // 初始将 containerMovements 元素的 HTML 内容清空
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+     <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} deposit ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+    `;
+
+    // afterbegin: 表示在父元素的开头插入新元素, 这里将html文本插入到 movements 元素的开头
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
+console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
