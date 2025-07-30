11107b7df63145cc0680b775ae39c060ed9914e7
ZEY <1010642166@qq.com>
2025-07-30 22:10:25 +0800

    11-Array-Bankist: Learn find() method

--- JavaScript/11-Arrays-Bankist/script.js
+++ JavaScript/11-Arrays-Bankist/script.js
@@ -404,17 +404,29 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
-const testData1 = [5, 2, 4, 1, 15, 8, 3];
-const testData2 = [16, 6, 10, 5, 6, 1, 4];
-const calcAverageHumanAge = (ages) => {
-  return ages
-    .map((age) => (age <= 2 ? age * 2 : age * 4 + 16))
-    .filter((age) => age >= 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-};
+// const testData1 = [5, 2, 4, 1, 15, 8, 3];
+// const testData2 = [16, 6, 10, 5, 6, 1, 4];
+// const calcAverageHumanAge = (ages) => {
+//   return ages
+//     .map((age) => (age <= 2 ? age * 2 : age * 4 + 16))
+//     .filter((age) => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// };
+//
+// const avg1 = calcAverageHumanAge(testData1);
+// console.log(avg1);
+// console.log("=====================================");
+// const avg2 = calcAverageHumanAge(testData2);
+// console.log(avg2);
 
-const avg1 = calcAverageHumanAge(testData1);
-console.log(avg1);
-console.log("=====================================");
-const avg2 = calcAverageHumanAge(testData2);
-console.log(avg2);
+// ------------------------------------ Find Method ------------------------------------ //
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// find 返回第一个匹配的元素
+const firstWithdrawal = movements.find((mov) => mov < 0);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(account);
