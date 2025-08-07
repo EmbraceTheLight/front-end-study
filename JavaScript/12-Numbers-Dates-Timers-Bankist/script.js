366902f008aee980ebe5ae54c3d11fd3a38738f0
ZEY <1010642166@qq.com>
2025-08-07 23:21:20 +0800

    12-Numbers-Dates-Timers-Bankist: Learn number's basic concepts and Number's methods, such as isFinite, isNaN, parseInt, parseFloat, etc.

--- JavaScript/12-Numbers-Dates-Timers-Bankist/script.js
+++ JavaScript/12-Numbers-Dates-Timers-Bankist/script.js
@@ -255,3 +255,36 @@ btnSort.addEventListener("click", function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+console.log(23 === 23.0);
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); // false
+
+console.log(Number("23"));
+console.log(+"23");
+
+console.log(Number.parseInt("30px", 10));
+console.log(Number.parseInt("31px", 5));
+console.log(Number.parseInt("e23", 10));
+console.log(Number.parseInt(" 2.5rem "));
+
+console.log(Number.parseFloat(" 2.5rem "));
+
+// Check if a value is NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("23"));
+console.log(Number.isNaN("abc"));
+console.log(isNaN("abc"));
+console.log(Number.isNaN(+"20X"));
+console.log(Number.isNaN(23 / 0));
+
+// Check if a value is number
+console.log(Number.isFinite(23 / 0));
+console.log(Number.isFinite(20));
+console.log(Number.isFinite("20"));
+console.log(Number.isFinite(+"20X"));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger("23"));
+console.log(Number.isInteger(23 / 0));
