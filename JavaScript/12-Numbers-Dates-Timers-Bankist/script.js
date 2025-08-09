700bb5b75ab948bed681047d59023a072727da61
ZEY <1010642166@qq.com>
2025-08-09 16:14:00 +0800

    12-Numbers-Dates-Timers-Bankist: Learn remainder operator in JS

--- JavaScript/12-Numbers-Dates-Timers-Bankist/script.js
+++ JavaScript/12-Numbers-Dates-Timers-Bankist/script.js
@@ -256,76 +256,106 @@ btnSort.addEventListener("click", function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(23 === 23.0);
-console.log(0.1 + 0.2); // 0.30000000000000004
-console.log(0.1 + 0.2 === 0.3); // false
-
-console.log(Number("23"));
-console.log(+"23");
-
-console.log(Number.parseInt("30px", 10));
-console.log(Number.parseInt("31px", 5));
-console.log(Number.parseInt("e23", 10));
-console.log(Number.parseInt(" 2.5rem "));
-
-console.log(Number.parseFloat(" 2.5rem "));
-
-// Check if a value is NaN
-console.log(Number.isNaN(20));
-console.log(Number.isNaN("23"));
-console.log(Number.isNaN("abc"));
-console.log(isNaN("abc"));
-console.log(Number.isNaN(+"20X"));
-console.log(Number.isNaN(23 / 0));
-
-// Check if a value is number
-console.log(Number.isFinite(23 / 0));
-console.log(Number.isFinite(20));
-console.log(Number.isFinite("20"));
-console.log(Number.isFinite(+"20X"));
-
-console.log(Number.isInteger(23));
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger("23"));
-console.log(Number.isInteger(23 / 0));
+// ------------------------------ Number ------------------------------//
+// console.log(23 === 23.0);
+// console.log(0.1 + 0.2); // 0.30000000000000004
+// console.log(0.1 + 0.2 === 0.3); // false
+//
+// console.log(Number("23"));
+// console.log(+"23");
+//
+// console.log(Number.parseInt("30px", 10));
+// console.log(Number.parseInt("31px", 5));
+// console.log(Number.parseInt("e23", 10));
+// console.log(Number.parseInt(" 2.5rem "));
+//
+// console.log(Number.parseFloat(" 2.5rem "));
+//
+// // Check if a value is NaN
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN("23"));
+// console.log(Number.isNaN("abc"));
+// console.log(isNaN("abc"));
+// console.log(Number.isNaN(+"20X"));
+// console.log(Number.isNaN(23 / 0));
+//
+// // Check if a value is number
+// console.log(Number.isFinite(23 / 0));
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite("20"));
+// console.log(Number.isFinite(+"20X"));
+//
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger("23"));
+// console.log(Number.isInteger(23 / 0));
 
 // ------------------------------ Math Methods ------------------------------//
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
+// console.log(Math.sqrt(25));
+// console.log(Math.sqrt("25"));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+//
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, "23", 11, 2));
+// console.log(Math.min(5, 18, "23px", 11, 2));
+//
+// console.log(Math.min(5, 18, 23, 11, 2));
+//
+// console.log(Math.PI * Number.parseFloat("10px") ** 2);
+//
+// console.log(Math.trunc(Math.random() * 6) + 1);
+//
+// // 生成范围 [min, max] 的随机整数
+// const randomInt = (min, max) =>
+//   Math.trunc(Math.floor(Math.random() * (max - min + 1)) + min);
+// // [0, 1) --> [0,max - min + 1) == [0, max - min] --> [0, max - min] + min --> [min, max]
+// console.log(randomInt(10, 20));
+//
+// // round method
+// console.log(Math.trunc(23.3));
+//
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+//
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+//
+// console.log(Math.floor(23.3));
+// console.log(Math.floor("23.9"));
+//
+// console.log(Math.floor(-23.3));
+//
+// // rounding decimals
+// console.log((2.7).toFixed(0));
+// console.log((2.7).toFixed(3));
+// console.log((2.345).toFixed(2));
+// console.log(+(2.345).toFixed(2));
 
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, "23", 11, 2));
-console.log(Math.min(5, 18, "23px", 11, 2));
+// ------------------------------ Remainder Operator ------------------------------//
+console.log(5 % 2);
+console.log(5 / 2);
+console.log(8 % 3);
+console.log(8 / 3);
 
-console.log(Math.min(5, 18, 23, 11, 2));
+console.log(6 % 2);
+console.log(6 / 2);
 
-console.log(Math.PI * Number.parseFloat("10px") ** 2);
+console.log(7 % 2);
+console.log(7 / 2);
 
-console.log(Math.trunc(Math.random() * 6) + 1);
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
 
-// 生成范围 [min, max] 的随机整数
-const randomInt = (min, max) =>
-  Math.trunc(Math.floor(Math.random() * (max - min + 1)) + min);
-// [0, 1) --> [0,max - min + 1) == [0, max - min] --> [0, max - min] + min --> [min, max]
-console.log(randomInt(10, 20));
-
-// round method
-console.log(Math.trunc(23.3));
-
-console.log(Math.round(23.3));
-console.log(Math.round(23.9));
-
-console.log(Math.ceil(23.3));
-console.log(Math.ceil(23.9));
-
-console.log(Math.floor(23.3));
-console.log(Math.floor("23.9"));
-
-console.log(Math.floor(-23.3));
-
-// rounding decimals
-console.log((2.7).toFixed(0));
-console.log((2.7).toFixed(3));
-console.log((2.345).toFixed(2));
-console.log(+(2.345).toFixed(2));
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = "orangeRed";
+    }
+    if (i % 3 === 0) {
+      row.style.backgroundColor = "lightBlue";
+    }
+  });
+});
