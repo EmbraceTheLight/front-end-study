a924b6145eb9a99f3ef89241df29d588c4e255e4
ZEY <1010642166@qq.com>
2025-08-16 17:14:59 +0800

    13-Advanced-DOM-Bankist: Learn add event listener and remove it

--- JavaScript/13-Advanced-DOM-Bankist/script.js
+++ JavaScript/13-Advanced-DOM-Bankist/script.js
@@ -143,3 +143,16 @@ btnScrollTo.addEventListener("click", function (e) {
   // Modern method to scroll to an element
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+// ------------------------------ Add && Remove Scrolling ------------------------------//
+const h1 = document.querySelector("h1");
+
+const alertH1 = function (e) {
+  alert("addEventListener: Great! You are reading the heading :D");
+};
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+// h1.onmouseover = function (e) {
+//   alert("addEventListener: Great! You are reading the heading :D");
+// };
