85b27ce19348c0d049d15b251ebc2a357b62127a
ZEY <1010642166@qq.com>
2025-08-26 23:46:58 +0800

    14-OOP: Learn about prototype inheritance and prototype chain

--- JavaScript/14-OOP/starter/script.js
+++ JavaScript/14-OOP/starter/script.js
@@ -46,5 +46,28 @@ console.log(Person.prototype.isPrototypeOf(Person));
 
 Person.prototype.species = "Homo Sapiens";
 console.log(jonas.species, matilda.species);
+
 console.log(jonas.hasOwnProperty("firstName"));
 console.log(jonas.hasOwnProperty("species"));
+
+// Object.prototype (top of the prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor === Person);
+console.dir(Person.prototype.constructor);
+
+// built-in array object's prototype
+const arr = [3, 6, 6, 4, 5, 6, 9, 3, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
