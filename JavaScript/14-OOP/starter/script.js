e7de101279f0f85a22cd9237c7b21abbdbf9680d
ZEY <1010642166@qq.com>
2025-08-27 23:37:38 +0800

    14-OOP: Learn ES6 Classes

--- JavaScript/14-OOP/starter/script.js
+++ JavaScript/14-OOP/starter/script.js
@@ -73,37 +73,72 @@
 // console.dir((x) => x + 1);
 
 // ------------------------------ Coding Challenge #1 ------------------------------//
-/*
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+// /*
+// 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+// 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+//
+// DATA CAR 1: 'BMW' going at 120 km/h
+// DATA CAR 2: 'Mercedes' going at 95 km/h
+//
+// GOOD LUCK 😀
+// */
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+//
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+// Car.prototype.break = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+//
+// const bmw = new Car("BMW", 120);
+// const mercedes = new Car("Mercedes", 95);
+//
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.break();
+// bmw.accelerate();
+//
+// mercedes.break();
 
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
+// ------------------------------ ES6 Classes ------------------------------//
+// class expression
+// const PersonCl = class{}
 
-GOOD LUCK 😀
-*/
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
-Car.prototype.break = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
 
-const bmw = new Car("BMW", 120);
-const mercedes = new Car("Mercedes", 95);
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.break();
-bmw.accelerate();
+const jessica = new PersonCl("Jessica", 1996);
+console.log(jessica);
+jessica.calcAge();
 
-mercedes.break();
+console.log(jessica.__proto__ === PersonCl.prototype);
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Class are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
