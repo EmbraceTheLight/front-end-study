59d48cae9445916d198f37d07dfe72f1aa8e43af
ZEY <1010642166@qq.com>
2025-09-01 23:59:13 +0800

    14-OOP: Learn the Encapsulation in class, about the private and public access modifiers

--- JavaScript/14-OOP/starter/script.js
+++ JavaScript/14-OOP/starter/script.js
@@ -428,3 +428,74 @@ jay.init("Jay", 2010, "Computer Science");
 console.log(jay);
 jay.introduce();
 jay.calcAge();
+
+// ------------------------------ Encapsulation   ------------------------------//
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// Also same for static fields/methods
+
+class Account {
+  // 1) Public fields(created on all instance instead of prototype)
+  locale = navigator.language;
+
+  // 2) Private fields(created on all instance instead of prototype)
+  #movements = [];
+  #pin;
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+
+    // protected property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+  }
+
+  // 3) Public methods
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+
+  static helper() {
+    console.log("Helper");
+  }
+
+  // 4) Private methods
+  #approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1._approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+
+// console.log(acc1.#movements); // Error: Private field '#movements' must be declared in an enclosing class
+// console.log(acc1.#pin); // Error: Private field '#movements' must be declared in an enclosing class
+Account.helper(); // Error: Static methods must be called on the class, not an instance of the class
