5ebd28d521b2d182f8e1a4db894515e3ce1b441f
ZEY <1010642166@qq.com>
2025-09-13 20:41:22 +0800

    16-Asynchronous: Learn call back hell

--- JavaScript/16-Asynchronous/script.js
+++ JavaScript/16-Asynchronous/script.js
@@ -11,7 +11,6 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 // ------------------------------ XMLHttpRequest ------------------------------ //
-
 const firstKeyVal = function (object) {
   return Object.entries(object);
 };
@@ -24,18 +23,43 @@ const getCurrencyName = function (countryInfo) {
   const currencies = firstKeyVal(countryInfo.currencies)[0];
   return currencies[0];
 };
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//
+//     // 取得第一条 currency 的 value
+//     const currency = firstKeyVal(data.currencies)[0][0];
+//     const html = `
+//   <article class="country">
+//           <img class="country__img" src="${data.flags.svg}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name.common}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+//             <p class="country__row"><span>🗣️</span>${getLanguageName(data)}</p>
+//             <p class="country__row"><span>💰</span>${getCurrencyName(data)}</p>
+//           </div>
+//         </article>
+//   `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = '1';
+//   });
+// };
+//
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData("People's Republic of China");
+// getCountryData('Germany');
 
-    // 取得第一条 currency 的 value
-    const currency = firstKeyVal(data.currencies)[0][0];
-    const html = `
-  <article class="country">
+// ------------------------------ call back hell ------------------------------ //
+const renderCountry = function (data, className = '') {
+  // 取得第一条 currency 的 value
+  const html = `
+  <article class="country ${className}" >
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -46,12 +70,37 @@ const getCountryData = function (country) {
           </div>
         </article>
   `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = '1';
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+
+const getCountryAndNeighbor = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // get neighbor country(2)
+    const [neighbor] = data.borders;
+
+    if (!neighbor) return;
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData("People's Republic of China");
-getCountryData('Germany');
+// getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('usa');
