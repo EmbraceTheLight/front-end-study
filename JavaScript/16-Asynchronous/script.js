942d6737fea569c26715fd22d097e30f30d1ead4
ZEY <1010642166@qq.com>
2025-09-14 19:34:09 +0800

    16-Asynchronous: Learn fetch API and the use of Promise.

--- JavaScript/16-Asynchronous/script.js
+++ JavaScript/16-Asynchronous/script.js
@@ -3,6 +3,29 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  // 取得第一条 currency 的 value
+  const html = `
+  <article class="country ${className}" >
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${getLanguageName(data)}</p>
+            <p class="country__row"><span>💰</span>${getCurrencyName(data)}</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = '1';
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = '1';
+};
+
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -56,51 +79,92 @@ const getCurrencyName = function (countryInfo) {
 // getCountryData('Germany');
 
 // ------------------------------ call back hell ------------------------------ //
-const renderCountry = function (data, className = '') {
-  // 取得第一条 currency 的 value
-  const html = `
-  <article class="country ${className}" >
-          <img class="country__img" src="${data.flags.svg}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${getLanguageName(data)}</p>
-            <p class="country__row"><span>💰</span>${getCurrencyName(data)}</p>
-          </div>
-        </article>
-  `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = '1';
-};
-
-const getCountryAndNeighbor = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    // Render country 1
-    renderCountry(data);
-
-    // get neighbor country(2)
-    const [neighbor] = data.borders;
-
-    if (!neighbor) return;
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
-    request2.send();
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
-      renderCountry(data2, 'neighbour');
-    });
-  });
-};
+//
+// const getCountryAndNeighbor = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//
+//     // Render country 1
+//     renderCountry(data);
+//
+//     // get neighbor country(2)
+//     const [neighbor] = data.borders;
+//
+//     if (!neighbor) return;
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+//     request2.send();
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
 
 // getCountryAndNeighbor('portugal');
-getCountryAndNeighbor('usa');
+// getCountryAndNeighbor('usa');
+
+// ------------------------------Promise and Fetch API ------------------------------ //
+// Old method to get data using XMLHttpRequest:
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+// Use fetch API to get data:
+// const request = fetch('https://restcountries.com/v3.1/name/portugal');
+// console.log(request);
+//
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(
+      response => response.json(),
+      // err => alert(err),
+    )
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
+
+      if (!neighbor) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbor}`);
+    })
+    .then(
+      response => response.json(),
+      // err => alert(err),
+    )
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = '1';
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
+
+getCountryData('fsdsf');
