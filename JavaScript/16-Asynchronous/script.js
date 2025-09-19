c7028af23ffc002271bb66f9ce01f38629faa659
ZEY <1010642166@qq.com>
2025-09-19 23:59:47 +0800

    16-Asynchronous: Learn the event loop and do some practice to understand how it works.

--- JavaScript/16-Asynchronous/script.js
+++ JavaScript/16-Asynchronous/script.js
@@ -301,5 +301,8 @@ btn.addEventListener('click', function () {
 console.log('Test Start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved Promise 1').then(res => console.log(res));
-
+Promise.resolve('Resolved Promise 2').then(res => {
+  for (let i = 0; i < 100000000000; i++) {}
+  console.log(res);
+});
 console.log('Test end');
