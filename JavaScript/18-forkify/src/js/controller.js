6f3b7c2f0ae7a960f144449bf34e91839f1643d2
ZEY <1010642166@qq.com>
2025-10-05 14:54:32 +0800

    feat(18-forkify): Listening hashchange and load events

--- JavaScript/18-forkify/src/js/controller.js
+++ JavaScript/18-forkify/src/js/controller.js
@@ -28,11 +28,14 @@ const renderSpinner = function (parentEl) {
 };
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
     // 1) loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      // 'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8906',
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -144,3 +147,8 @@ const showRecipe = async function () {
   }
 };
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
