bd5db6bc6b47edb80df766f0fc522425eeb7caf1
ZEY <1010642166@qq.com>
2025-10-14 23:53:12 +0800

    feat(18-forkify): Add controller for add new recipe

--- JavaScript/18-forkify/src/js/controller.js
+++ JavaScript/18-forkify/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultView from './views/resultView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -100,6 +101,12 @@ const controlBookmarks = function (){
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlAddRecipe = function (newRecipe){
+  console.log(newRecipe);
+
+  // Upload new recipe data
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -107,8 +114,9 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
-// init();
+init();
 
 const clearBookmarks = function (){
   localStorage.clear('bookmarks');
